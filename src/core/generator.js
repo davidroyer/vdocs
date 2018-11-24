a3fafa0493ba70f1ca4071b7009c05b6d622ffc0
David Royer <droyer01@gmail.com>
2018-11-24 03:56:00 -0500

    fix(generator.js): Fixed `fs` error by importing `fs`

--- src/core/generator.js
+++ src/core/generator.js
@@ -1,6 +1,4 @@
-import fs from "fs";
-
-const buildDir = "./docs/components";
+import fs from "fs-extra";
 
 export const TEST_ME = "TEST STRING";
 
@@ -16,71 +14,3 @@ export function createComponentsNavFile(config, navArray) {
     console.log("Cannot write file ", e);
   }
 }
-
-export function propsIterator(obj) {
-  let propsContent = ``;
-  for (const prop in obj) {
-    const { description, type } = obj[prop];
-    propsContent += `- \`${prop}\` ***${type.name.capitalize()}***
-    
-    ${description}
-
-`;
-  }
-  return propsContent;
-}
-
-export function defaultIterator(obj) {
-  let content = ``;
-  for (const propKey in obj) {
-    const info = obj[propKey];
-    content += `- \`${propKey}\`
-${info.description}
-
-`;
-  }
-  return content;
-}
-
-export function createJsonFile(content) {
-  // console.log(content);
-  try {
-    fs.writeFileSync("OUTPUT.json", JSON.stringify(content), "utf8");
-  } catch (e) {
-    console.log("Cannot write file ", e);
-  }
-}
-
-export function createMarkdownFile(filename, mdContent) {
-  // console.log(content);
-  try {
-    fs.writeFileSync(`${buildDir}/${filename}`, mdContent, "utf8");
-  } catch (e) {
-    console.log("Cannot write file ", e);
-  }
-}
-
-export function createMarkdownContent(content) {
-  const { displayName, description, props, events, methods, slots } = content;
-
-  const mdDocContent = `# ${displayName}
-${description}
-
-## Props
-
-${propsIterator(props)}
-
-## Events
-
-${defaultIterator(events)}
-
-## Slots
-
-${defaultIterator(slots)}
-`;
-  return mdDocContent;
-}
-
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-};
